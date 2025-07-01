8c3d81664582a92ddb074dc3e5685ce5065e02a5
Xinwei Liu <nadialiuwei@gmail.com>
2025-07-01 21:26:41 +1000

    runkperf/data/daemonset: Remove list processing logic during DaemonSet creation

--- contrib/cmd/runkperf/commands/data/daemonsets/daemonset.go
+++ contrib/cmd/runkperf/commands/data/daemonsets/daemonset.go
@@ -86,17 +86,6 @@ var daemonsetAddCommand = cli.Command{
 			return err
 		}
 
-		// Check if the daemonset already exists
-		labelSelector := fmt.Sprintf("app=%s, dsName in (%s)", appLabel, dsName)
-		daemonSets, err := listDaemonsets(clientset, labelSelector, namespace)
-
-		if err != nil {
-			return fmt.Errorf("failed to list daemonsets: %v while checking for existence", err)
-		}
-		if len(daemonSets.Items) > 0 {
-			return fmt.Errorf("daemonset with name prefix %s already exists in namespace %s", dsName, namespace)
-		}
-
 		// Create the daemonsets
 		err = createDaemonsets(clientset, namespace, dsName, count)
 		if err != nil {
