5ae6f055e02881211ad7439aa05942bbb55c8db2
Liyu Ma <maliyu@microsoft.com>
2026-03-17 10:31:11 +1000

    Add commands for event and secret data ingestion

---
+++ contrib/cmd/runkperf/commands/data/events/event.go
@@ -0,0 +1,410 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+package events
+
+import (
+	"context"
+	"crypto/rand"
+	"fmt"
+	"math/big"
+	"os"
+	"strings"
+	"text/tabwriter"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+
+	"github.com/Azure/kperf/cmd/kperf/commands/utils"
+	"github.com/Azure/kperf/contrib/cmd/runkperf/commands/data"
+
+	"github.com/urfave/cli"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+var appLabel = "runkperf"
+
+var Command = cli.Command{
+	Name:      "event",
+	ShortName: "ev",
+	Usage:     "Manage events",
+	Flags: []cli.Flag{
+		cli.StringFlag{
+			Name:  "kubeconfig",
+			Usage: "Path to the kubeconfig file",
+			Value: utils.DefaultKubeConfigPath,
+		},
+		cli.StringFlag{
+			Name:  "namespace",
+			Usage: "Namespace to use with commands. If the namespace does not exist, it will be created.",
+			Value: "default",
+		},
+	},
+	Subcommands: []cli.Command{
+		eventAddCommand,
+		eventDelCommand,
+		eventListCommand,
+	},
+}
+
+var eventAddCommand = cli.Command{
+	Name:      "add",
+	Usage:     "Add events set",
+	ArgsUsage: "NAME of the events set",
+	Flags: []cli.Flag{
+		cli.IntFlag{
+			Name:  "total",
+			Usage: "Total number of events to create",
+			Value: 10,
+		},
+		cli.IntFlag{
+			Name:  "group-size",
+			Usage: "Number of events to create in parallel per batch",
+			Value: 30,
+		},
+		cli.StringFlag{
+			Name:  "reason",
+			Usage: "The reason string for the events (e.g. ScaleUp, FailedScheduling)",
+			Value: "BenchmarkEvent",
+		},
+		cli.StringFlag{
+			Name:  "type",
+			Usage: "The type of the events: Normal or Warning",
+			Value: "Normal",
+		},
+		cli.IntFlag{
+			Name:  "message-size",
+			Usage: "The size of the event message in bytes (default targets ~5KB total event size; k8s metadata overhead is ~500-700 bytes)",
+			Value: 5000,
+		},
+		cli.StringFlag{
+			Name:  "involved-object-kind",
+			Usage: "The kind of the involved object (e.g. Pod, Node, Deployment)",
+			Value: "Pod",
+		},
+		cli.StringFlag{
+			Name:  "involved-object-name",
+			Usage: "The name of the involved object. If empty, a name will be generated.",
+			Value: "",
+		},
+		cli.Float64Flag{
+			Name:  "qps",
+			Usage: "QPS for the Kubernetes client rate limiter to control event operations",
+			Value: 100,
+		},
+		cli.IntFlag{
+			Name:  "burst",
+			Usage: "Burst for the Kubernetes client rate limiter to control event operations",
+			Value: 200,
+		},
+	},
+	Action: func(cliCtx *cli.Context) error {
+		if cliCtx.NArg() != 1 {
+			return fmt.Errorf("required only one argument as events set name: %v", cliCtx.Args())
+		}
+		evName := strings.TrimSpace(cliCtx.Args().Get(0))
+		if len(evName) == 0 {
+			return fmt.Errorf("required non-empty event set name")
+		}
+
+		kubeCfgPath := cliCtx.GlobalString("kubeconfig")
+		namespace := cliCtx.GlobalString("namespace")
+		total := cliCtx.Int("total")
+		groupSize := cliCtx.Int("group-size")
+		reason := cliCtx.String("reason")
+		eventType := cliCtx.String("type")
+		messageSize := cliCtx.Int("message-size")
+		involvedObjectKind := cliCtx.String("involved-object-kind")
+		involvedObjectName := cliCtx.String("involved-object-name")
+		qps := float32(cliCtx.Float64("qps"))
+		burst := cliCtx.Int("burst")
+
+		if total <= 0 {
+			return fmt.Errorf("total must be greater than 0")
+		}
+		if groupSize <= 0 {
+			return fmt.Errorf("group-size must be greater than 0")
+		}
+		if groupSize > total {
+			return fmt.Errorf("group-size must be less than or equal to total")
+		}
+		if eventType != "Normal" && eventType != "Warning" {
+			return fmt.Errorf("type must be either Normal or Warning")
+		}
+		if messageSize <= 0 {
+			return fmt.Errorf("message-size must be greater than 0")
+		}
+
+		clientset, err := data.NewClientsetWithRateLimiter(kubeCfgPath, qps, burst)
+		if err != nil {
+			return err
+		}
+
+		err = prepareNamespace(clientset, namespace)
+		if err != nil {
+			return err
+		}
+
+		err = createEvents(clientset, namespace, evName, total, groupSize, reason, eventType, messageSize, involvedObjectKind, involvedObjectName)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("Created %d events (set=%s, reason=%s, type=%s) in namespace %s\n",
+			total, evName, reason, eventType, namespace)
+		return nil
+	},
+}
+
+var eventDelCommand = cli.Command{
+	Name:      "delete",
+	ShortName: "del",
+	ArgsUsage: "NAME",
+	Usage:     "Delete an events set",
+	Action: func(cliCtx *cli.Context) error {
+		if cliCtx.NArg() != 1 {
+			return fmt.Errorf("required only one events set name")
+		}
+		evName := strings.TrimSpace(cliCtx.Args().Get(0))
+		if len(evName) == 0 {
+			return fmt.Errorf("required non-empty events set name")
+		}
+
+		namespace := cliCtx.GlobalString("namespace")
+		kubeCfgPath := cliCtx.GlobalString("kubeconfig")
+		labelSelector := fmt.Sprintf("app=%s,evName=%s", appLabel, evName)
+
+		clientset, err := data.NewClientset(kubeCfgPath)
+		if err != nil {
+			return err
+		}
+
+		err = deleteEvents(clientset, labelSelector, namespace)
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("Deleted events set %s in %s namespace\n", evName, namespace)
+		return nil
+	},
+}
+
+var eventListCommand = cli.Command{
+	Name:  "list",
+	Usage: "List generated event sets. Lists all if no arguments are given; otherwise, provide event set names separated by spaces.",
+	Action: func(cliCtx *cli.Context) error {
+		namespace := cliCtx.GlobalString("namespace")
+		kubeCfgPath := cliCtx.GlobalString("kubeconfig")
+		clientset, err := data.NewClientset(kubeCfgPath)
+		if err != nil {
+			return err
+		}
+
+		const (
+			minWidth = 1
+			tabWidth = 12
+			padding  = 3
+			padChar  = ' '
+			flags    = 0
+		)
+		tw := tabwriter.NewWriter(os.Stdout, minWidth, tabWidth, padding, padChar, flags)
+		fmt.Fprintln(tw, "NAME\tTYPE\tREASON\tTOTAL\t")
+
+		var labelSelector string
+		if cliCtx.NArg() == 0 {
+			labelSelector = fmt.Sprintf("app=%s,evName", appLabel)
+		} else {
+			args := cliCtx.Args()
+			namesStr := strings.Join(args, ",")
+			labelSelector = fmt.Sprintf("app=%s, evName in (%s)", appLabel, namesStr)
+		}
+
+		evMap, err := listEventsByName(clientset, labelSelector, namespace)
+		if err != nil {
+			return err
+		}
+
+		for name, info := range evMap {
+			fmt.Fprintf(tw, "%s\t%s\t%s\t%d\n",
+				name,
+				info.eventType,
+				info.reason,
+				info.total,
+			)
+		}
+		return tw.Flush()
+	},
+}
+
+type eventSetInfo struct {
+	eventType string
+	reason    string
+	total     int
+}
+
+func prepareNamespace(clientset *kubernetes.Clientset, namespace string) error {
+	if namespace == "" {
+		return fmt.Errorf("namespace cannot be empty")
+	}
+
+	if namespace == "default" {
+		return nil
+	}
+
+	_, err := clientset.CoreV1().Namespaces().Create(context.TODO(), &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: namespace,
+		},
+	}, metav1.CreateOptions{})
+	if err != nil {
+		if errors.IsAlreadyExists(err) {
+			return nil
+		}
+		return fmt.Errorf("failed to create namespace %s: %v", namespace, err)
+	}
+	return nil
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randString(n int) (string, error) {
+	if n <= 0 {
+		return "", fmt.Errorf("length must be positive")
+	}
+
+	b := make([]rune, n)
+	for i := range b {
+		random, err := rand.Int(rand.Reader, big.NewInt(int64(len(letterRunes))))
+		if err != nil {
+			return "", fmt.Errorf("error generating random number: %w", err)
+		}
+		b[i] = letterRunes[int(random.Int64())]
+	}
+	return string(b), nil
+}
+
+func createEvents(clientset *kubernetes.Clientset, namespace, evName string, total, groupSize int, reason, eventType string, messageSize int, involvedObjectKind, involvedObjectName string) error {
+	now := time.Now()
+
+	for i := 0; i < total; i += groupSize {
+		g := new(errgroup.Group)
+		for j := i; j < i+groupSize && j < total; j++ {
+			idx := j
+			g.Go(func() error {
+				name := fmt.Sprintf("%s-ev-%s-%d", appLabel, evName, idx)
+
+				message, err := randString(messageSize)
+				if err != nil {
+					return fmt.Errorf("failed to generate random message for event %s: %v", name, err)
+				}
+
+				objName := involvedObjectName
+				if objName == "" {
+					objName = fmt.Sprintf("%s-obj-%d", evName, idx)
+				}
+
+				event := &corev1.Event{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      name,
+						Namespace: namespace,
+						Labels: map[string]string{
+							"app":    appLabel,
+							"evName": evName,
+						},
+					},
+					InvolvedObject: corev1.ObjectReference{
+						Kind:      involvedObjectKind,
+						Name:      objName,
+						Namespace: namespace,
+					},
+					Reason:         reason,
+					Message:        message,
+					Type:           eventType,
+					Count:          1,
+					FirstTimestamp: metav1.NewTime(now),
+					LastTimestamp:  metav1.NewTime(now),
+					Source: corev1.EventSource{
+						Component: "runkperf-bench",
+					},
+				}
+
+				_, err = clientset.CoreV1().Events(namespace).Create(context.TODO(), event, metav1.CreateOptions{})
+				if err != nil {
+					return fmt.Errorf("failed to create event %s: %v", name, err)
+				}
+				return nil
+			})
+		}
+		if err := g.Wait(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func deleteEvents(clientset *kubernetes.Clientset, labelSelector, namespace string) error {
+	eventList, err := listEvents(clientset, labelSelector, namespace)
+	if err != nil {
+		return err
+	}
+
+	if len(eventList.Items) == 0 {
+		return fmt.Errorf("no events set found in namespace: %s", namespace)
+	}
+
+	n, batch := len(eventList.Items), 300
+	for i := 0; i < n; i += batch {
+		g := new(errgroup.Group)
+		for j := i; j < i+batch && j < n; j++ {
+			idx := j
+			g.Go(func() error {
+				err := clientset.CoreV1().Events(namespace).Delete(context.TODO(), eventList.Items[idx].Name, metav1.DeleteOptions{})
+				if err != nil && !errors.IsNotFound(err) {
+					return fmt.Errorf("failed to delete event %s: %v", eventList.Items[idx].Name, err)
+				}
+				return nil
+			})
+		}
+		if err := g.Wait(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func listEvents(clientset *kubernetes.Clientset, labelSelector, namespace string) (*corev1.EventList, error) {
+	eventList, err := clientset.CoreV1().Events(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: labelSelector})
+	if err != nil {
+		return nil, fmt.Errorf("failed to list events: %v", err)
+	}
+	return eventList, nil
+}
+
+func listEventsByName(clientset *kubernetes.Clientset, labelSelector, namespace string) (map[string]*eventSetInfo, error) {
+	eventList, err := listEvents(clientset, labelSelector, namespace)
+	if err != nil {
+		return nil, err
+	}
+
+	evMap := make(map[string]*eventSetInfo)
+	for _, ev := range eventList.Items {
+		name, ok := ev.Labels["evName"]
+		if !ok {
+			continue
+		}
+
+		info, ok := evMap[name]
+		if !ok {
+			info = &eventSetInfo{
+				eventType: ev.Type,
+				reason:    ev.Reason,
+			}
+			evMap[name] = info
+		}
+		info.total++
+	}
+	return evMap, nil
+}
---
+++ contrib/cmd/runkperf/commands/data/secrets/secret.go
@@ -0,0 +1,389 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+package secrets
+
+import (
+	"context"
+	"crypto/rand"
+	"fmt"
+	"math/big"
+	"os"
+	"strconv"
+	"strings"
+	"text/tabwriter"
+
+	"golang.org/x/sync/errgroup"
+
+	"github.com/Azure/kperf/cmd/kperf/commands/utils"
+	"github.com/Azure/kperf/contrib/cmd/runkperf/commands/data"
+
+	"github.com/urfave/cli"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+var appLabel = "runkperf"
+
+var Command = cli.Command{
+	Name:      "secret",
+	ShortName: "sec",
+	Usage:     "Manage secrets",
+	Flags: []cli.Flag{
+		cli.StringFlag{
+			Name:  "kubeconfig",
+			Usage: "Path to the kubeconfig file",
+			Value: utils.DefaultKubeConfigPath,
+		},
+		cli.StringFlag{
+			Name:  "namespace",
+			Usage: "Namespace to use with commands. If the namespace does not exist, it will be created.",
+			Value: "default",
+		},
+	},
+	Subcommands: []cli.Command{
+		secretAddCommand,
+		secretDelCommand,
+		secretListCommand,
+	},
+}
+
+var secretAddCommand = cli.Command{
+	Name:      "add",
+	Usage:     "Add secrets set",
+	ArgsUsage: "NAME of the secrets set",
+	Flags: []cli.Flag{
+		cli.IntFlag{
+			Name:  "size",
+			Usage: "The size of each secret value (Unit: Byte)",
+			Value: 100,
+		},
+		cli.IntFlag{
+			Name:  "group-size",
+			Usage: "The number of secrets to create in parallel per batch",
+			Value: 30,
+		},
+		cli.IntFlag{
+			Name:  "total",
+			Usage: "Total number of secrets to create",
+			Value: 10,
+		},
+		cli.Float64Flag{
+			Name:  "qps",
+			Usage: "QPS for the Kubernetes client rate limiter to control secret operations",
+			Value: 100,
+		},
+		cli.IntFlag{
+			Name:  "burst",
+			Usage: "Burst for the Kubernetes client rate limiter to control secret operations",
+			Value: 200,
+		},
+	},
+	Action: func(cliCtx *cli.Context) error {
+		if cliCtx.NArg() != 1 {
+			return fmt.Errorf("required only one argument as secrets set name: %v", cliCtx.Args())
+		}
+		secName := strings.TrimSpace(cliCtx.Args().Get(0))
+		if len(secName) == 0 {
+			return fmt.Errorf("required non-empty secret set name")
+		}
+
+		kubeCfgPath := cliCtx.GlobalString("kubeconfig")
+		size := cliCtx.Int("size")
+		groupSize := cliCtx.Int("group-size")
+		total := cliCtx.Int("total")
+
+		err := checkSecretParams(size, groupSize, total)
+		if err != nil {
+			return err
+		}
+
+		namespace := cliCtx.GlobalString("namespace")
+		qps := float32(cliCtx.Float64("qps"))
+		burst := cliCtx.Int("burst")
+
+		clientset, err := data.NewClientsetWithRateLimiter(kubeCfgPath, qps, burst)
+		if err != nil {
+			return err
+		}
+
+		err = prepareNamespace(clientset, namespace)
+		if err != nil {
+			return err
+		}
+
+		err = createSecrets(clientset, namespace, secName, size, groupSize, total)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("Created secret %s with size %d B, group-size %d, total %d\n", secName, size, groupSize, total)
+		return nil
+	},
+}
+
+var secretDelCommand = cli.Command{
+	Name:      "delete",
+	ShortName: "del",
+	ArgsUsage: "NAME",
+	Usage:     "Delete a secrets set",
+	Action: func(cliCtx *cli.Context) error {
+		if cliCtx.NArg() != 1 {
+			return fmt.Errorf("required only one secrets set name")
+		}
+		secName := strings.TrimSpace(cliCtx.Args().Get(0))
+		if len(secName) == 0 {
+			return fmt.Errorf("required non-empty secrets set name")
+		}
+
+		namespace := cliCtx.GlobalString("namespace")
+		kubeCfgPath := cliCtx.GlobalString("kubeconfig")
+		labelSelector := fmt.Sprintf("app=%s,secName=%s", appLabel, secName)
+
+		clientset, err := data.NewClientset(kubeCfgPath)
+		if err != nil {
+			return err
+		}
+
+		err = deleteSecrets(clientset, labelSelector, namespace)
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("Deleted secrets set %s in %s namespace\n", secName, namespace)
+		return nil
+	},
+}
+
+var secretListCommand = cli.Command{
+	Name:      "list",
+	Usage:     "List generated secrets. Lists all if no arguments are given; otherwise, provide secret set names separated by spaces.",
+	ArgsUsage: "NAME",
+	Action: func(cliCtx *cli.Context) error {
+		namespace := cliCtx.GlobalString("namespace")
+		kubeCfgPath := cliCtx.GlobalString("kubeconfig")
+		clientset, err := data.NewClientset(kubeCfgPath)
+		if err != nil {
+			return err
+		}
+
+		const (
+			minWidth = 1
+			tabWidth = 12
+			padding  = 3
+			padChar  = ' '
+			flags    = 0
+		)
+		tw := tabwriter.NewWriter(os.Stdout, minWidth, tabWidth, padding, padChar, flags)
+		fmt.Fprintln(tw, "NAME\tSIZE\tGROUP_SIZE\tTOTAL\t")
+
+		var labelSelector string
+		if cliCtx.NArg() == 0 {
+			labelSelector = fmt.Sprintf("app=%s,secName", appLabel)
+		} else {
+			args := cliCtx.Args()
+			namesStr := strings.Join(args, ",")
+			labelSelector = fmt.Sprintf("app=%s, secName in (%s)", appLabel, namesStr)
+		}
+
+		secMap := make(map[string][]int)
+		err = listSecretsByName(clientset, labelSelector, namespace, secMap)
+		if err != nil {
+			return err
+		}
+
+		for key, value := range secMap {
+			fmt.Fprintf(tw, "%s\t%d\t%d\t%d\n",
+				key,
+				value[0],
+				value[1],
+				value[2],
+			)
+		}
+		return tw.Flush()
+	},
+}
+
+func prepareNamespace(clientset *kubernetes.Clientset, namespace string) error {
+	if namespace == "" {
+		return fmt.Errorf("namespace cannot be empty")
+	}
+
+	if namespace == "default" {
+		return nil
+	}
+
+	_, err := clientset.CoreV1().Namespaces().Create(context.TODO(), &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: namespace,
+		},
+	}, metav1.CreateOptions{})
+	if err != nil {
+		if errors.IsAlreadyExists(err) {
+			return nil
+		}
+		return fmt.Errorf("failed to create namespace %s: %v", namespace, err)
+	}
+	return nil
+}
+
+func checkSecretParams(size int, groupSize int, total int) error {
+	if size <= 0 {
+		return fmt.Errorf("size must be greater than 0")
+	}
+	if groupSize <= 0 {
+		return fmt.Errorf("group-size must be greater than 0")
+	}
+	if total <= 0 {
+		return fmt.Errorf("total amount must be greater than 0")
+	}
+	if groupSize > total {
+		return fmt.Errorf("group-size must be less than or equal to total")
+	}
+	return nil
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+func randBytes(n int) ([]byte, error) {
+	if n <= 0 {
+		return nil, fmt.Errorf("length must be positive")
+	}
+
+	b := make([]byte, n)
+	for i := range b {
+		random, err := rand.Int(rand.Reader, big.NewInt(int64(len(letterRunes))))
+		if err != nil {
+			return nil, fmt.Errorf("error generating random number: %w", err)
+		}
+		b[i] = byte(letterRunes[int(random.Int64())])
+	}
+	return b, nil
+}
+
+func createSecrets(clientset *kubernetes.Clientset, namespace string, secName string, size int, groupSize int, total int) error {
+	for i := 0; i < total; i += groupSize {
+		ownerID := i
+		g := new(errgroup.Group)
+		for j := i; j < i+groupSize && j < total; j++ {
+			idx := j
+			g.Go(func() error {
+				name := fmt.Sprintf("%s-sec-%s-%d", appLabel, secName, idx)
+
+				data, err := randBytes(size)
+				if err != nil {
+					return fmt.Errorf("failed to generate random data for secret %s: %v", name, err)
+				}
+
+				secret := &corev1.Secret{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: name,
+						Labels: map[string]string{
+							"ownerID": strconv.Itoa(ownerID),
+							"app":     appLabel,
+							"secName": secName,
+						},
+					},
+					Type: corev1.SecretTypeOpaque,
+					Data: map[string][]byte{
+						"data": data,
+					},
+				}
+
+				_, err = clientset.CoreV1().Secrets(namespace).Create(context.TODO(), secret, metav1.CreateOptions{})
+				if err != nil {
+					return fmt.Errorf("failed to create secret %s: %v", name, err)
+				}
+				return nil
+			})
+		}
+		if err := g.Wait(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func deleteSecrets(clientset *kubernetes.Clientset, labelSelector string, namespace string) error {
+	secrets, err := listSecrets(clientset, labelSelector, namespace)
+	if err != nil {
+		return err
+	}
+
+	if len(secrets.Items) == 0 {
+		return fmt.Errorf("no secrets set found in namespace: %s", namespace)
+	}
+
+	n, batch := len(secrets.Items), 300
+	for i := 0; i < n; i += batch {
+		g := new(errgroup.Group)
+		for j := i; j < i+batch && j < n; j++ {
+			idx := j
+			g.Go(func() error {
+				err := clientset.CoreV1().Secrets(namespace).Delete(context.TODO(), secrets.Items[idx].Name, metav1.DeleteOptions{})
+				if err != nil && !errors.IsNotFound(err) {
+					return fmt.Errorf("failed to delete secret %s: %v", secrets.Items[idx].Name, err)
+				}
+				return nil
+			})
+		}
+		if err := g.Wait(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func listSecrets(clientset *kubernetes.Clientset, labelSelector string, namespace string) (*corev1.SecretList, error) {
+	secrets, err := clientset.CoreV1().Secrets(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: labelSelector})
+	if err != nil {
+		return nil, fmt.Errorf("failed to list secrets: %v", err)
+	}
+	return secrets, nil
+}
+
+func listSecretsByName(clientset *kubernetes.Clientset, labelSelector string, namespace string, secMap map[string][]int) error {
+	secrets, err := listSecrets(clientset, labelSelector, namespace)
+	if err != nil {
+		return err
+	}
+
+	for _, sec := range secrets.Items {
+		name, ok := sec.Labels["secName"]
+		if !ok {
+			return fmt.Errorf("failed to find the secName of secret %s", sec.Name)
+		}
+
+		_, ok = secMap[name]
+		if !ok {
+			// Initialize: size, group-size, total
+			secMap[name] = []int{0, 0, 0}
+
+			if data, exists := sec.Data["data"]; exists {
+				secMap[name][0] = len(data)
+			}
+		}
+
+		secMap[name][2]++
+
+		if secMap[name][1] != 0 {
+			continue
+		}
+
+		ownerID, ok := sec.Labels["ownerID"]
+		if !ok {
+			return fmt.Errorf("failed to find the ownerID of secret %s", name)
+		}
+
+		if ownerIDInt, err := strconv.Atoi(ownerID); err == nil {
+			if ownerIDInt > secMap[name][1] {
+				secMap[name][1] = ownerIDInt
+			}
+		} else {
+			return fmt.Errorf("failed to convert ownerID %s to int: %v", ownerID, err)
+		}
+	}
+	return nil
+}
